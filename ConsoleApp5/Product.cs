5488b974ff0fdb90604458d9d9b9ce588929c11e
Aводник Aводник <avodnik84@gmail.com>
2022-05-18 19:55:11 +0200

    18.05.2022 1

---
+++ ConsoleApp5/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+ public class Product
+{
+	public string Name { get; } 
+	public double Prise { get; }
+	public double Weight { get; }
+	public  Product (string name, double prise, double weight )
+	{
+		Name = name;
+		if (prise < 0 || weight < 0)
+			throw new ArgumentException();
+		Prise = prise;
+		Weight = weight;
+		
+	}
+
+
+	static void Main(string[] args)
+    {
+
+    }
+
+}
